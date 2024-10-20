3ad3a29a2a6a7922b35a37ca13d257a31d088022
Shruti Patharkar <shrutipatharkar1122@gmail.com>
2024-10-20 22:29:13 +0530

    Add files via upload

---
+++ Polymorphism(virtual function).cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+  public:
+  virtual void print(){
+    cout<<"Base Function"<<endl;
+  }
+};
+
+class Derived : public Base{
+  public:
+  void print(){
+    cout<<"Derived Function"<<endl;
+  }
+};
+
+int main(){
+  Derived derived1;
+  
+  //pointer of Base type that Points to derived1
+  Base*base1 =&derived1;
+  
+  //Calls member function of Derived class
+  base1->print();
+  
+  return 0;
+  
+}
